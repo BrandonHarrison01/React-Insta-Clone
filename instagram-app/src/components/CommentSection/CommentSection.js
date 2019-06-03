4f107bd42b754d2c8279fea80df1b465d6286006
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-06-03 16:12:32 -0600

    WIP directory and imports built

---
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CommentSection(){
+    return
+}
+
+export default CommentSection;
---
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function PostContainer(){
+    return
+}
+
+export default PostContainer;
