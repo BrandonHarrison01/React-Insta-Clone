026cd520dccc09a8392b363b3a5fb77ba558e0ce
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-06-04 15:53:46 -0600

    WIP little behind ... but progress

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -3,16 +3,24 @@ import './App.css';
 
 import SearchBar from './components/SearchBar/SearchBar';
 import PostContainer from './components/PostContainer/PostContainer';
-import CommentSection from './components/CommentSection/CommentSection';
+import dummyData from './dummy-data';
 
-function App() {
-  return (
-    <div className="App">
-      <SearchBar />
-      {/* <PostContainer />
-      <CommentSection /> */}
-    </div>
-  );
+class App extends React.Component {
+  state={
+      dummyData: dummyData
+  }
+
+  render() {
+    console.log(this.state.dummyData.id)
+    return (
+      <div className="App">
+        <SearchBar />
+        {this.state.dummyData.map((post) => (
+            <PostContainer postProp={post} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
-function PostContainer(){
-    return
+import CommentSection from '../CommentSection/CommentSection';
+
+function PostContainer(props){
+    return(
+        <div>
+            <div>
+                <img src={props.postProp.thumbnailUrl}/>
+                <h3>{props.postProp.username}</h3>
+            </div>
+            <img src={props.postProp.imageUrl}/>
+            <h3>{props.postProp.likes} likes</h3>
+            {/* <CommentSection /> */}
+        </div>
+    )
 }
 
 export default PostContainer;
